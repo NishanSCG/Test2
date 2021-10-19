b4c24af36d040b3f144bcc031ec6f1297332c044
NishanSCG <nishans@scg.com>
2021-10-19 12:31:20 +0530

    7 commit

--- JPA2demo/src/main/java/com/jpa2/demo/controller/CollegeController.java
+++ JPA2demo/src/main/java/com/jpa2/demo/controller/CollegeController.java
@@ -42,6 +42,10 @@ public class CollegeController {
 	// Manoj Kumar
 	// Nihal Kunder
 
+	// Nishan siriyan
+	// Manoj Kumar
+	// Nihal Kunder
+
 	@Autowired
 	private CollegeMapperImpl mapperClass;
 
